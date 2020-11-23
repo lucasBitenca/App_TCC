b43c608d9644e69baa0c75466ca6617942fc0425
lucasBitenca <42593403+lucasBitenca@users.noreply.github.com>
2020-11-23 20:07:39 -0300

    Adicionar arquivos de projeto.

---
+++ App_Final/App_Final/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App_Final.Services;
+using App_Final.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Final
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App_Final/App_Final/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using App_Final.ViewModels;
+using App_Final.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App_Final
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}

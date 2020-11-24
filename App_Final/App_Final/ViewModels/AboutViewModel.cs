dfa834525d944d1885cede9b09f2bcab9826af94
lucasBitenca <42593403+lucasBitenca@users.noreply.github.com>
2020-11-23 21:09:45 -0300

    alteração da tela "about".

--- App_Final/App_Final/ViewModels/AboutViewModel.cs
+++ App_Final/App_Final/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace App_Final.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/lucasBitenca/App_TCC/blob/master/README.md"));
         }
 
         public ICommand OpenWebCommand { get; }

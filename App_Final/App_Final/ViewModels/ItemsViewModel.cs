27eacaa3e013a572d1628e3ebbda69e74157db87
lucasBitenca <42593403+lucasBitenca@users.noreply.github.com>
2020-11-30 22:02:45 -0300

    Alteração da página home.

--- App_Final/App_Final/ViewModels/ItemsViewModel.cs
+++ App_Final/App_Final/ViewModels/ItemsViewModel.cs
@@ -1,84 +1,25 @@
 ﻿using App_Final.Models;
 using App_Final.Views;
 using System;
+using System.Windows.Input;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using Xamarin.Essentials;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace App_Final.ViewModels
 {
-    public class ItemsViewModel : BaseViewModel
+    public class HomeModel : BaseViewModel
     {
-        private Item _selectedItem;
 
-        public ObservableCollection<Item> Items { get; }
-        public Command LoadItemsCommand { get; }
-        public Command AddItemCommand { get; }
-        public Command<Item> ItemTapped { get; }
-
-        public ItemsViewModel()
+       
+        public HomeModel()
         {
-            Title = "Browse";
-            Items = new ObservableCollection<Item>();
-            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
-
-            ItemTapped = new Command<Item>(OnItemSelected);
-
-            AddItemCommand = new Command(OnAddItem);
+            Title = "Home";
+            AbrirHome = new Command(async () => await Browser.OpenAsync("http://104.41.11.135:1880/ui/"));
         }
+        public ICommand AbrirHome { get; }
 
-        async Task ExecuteLoadItemsCommand()
-        {
-            IsBusy = true;
-
-            try
-            {
-                Items.Clear();
-                var items = await DataStore.GetItemsAsync(true);
-                foreach (var item in items)
-                {
-                    Items.Add(item);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-
-        public void OnAppearing()
-        {
-            IsBusy = true;
-            SelectedItem = null;
-        }
-
-        public Item SelectedItem
-        {
-            get => _selectedItem;
-            set
-            {
-                SetProperty(ref _selectedItem, value);
-                OnItemSelected(value);
-            }
-        }
-
-        private async void OnAddItem(object obj)
-        {
-            await Shell.Current.GoToAsync(nameof(NewItemPage));
-        }
-
-        async void OnItemSelected(Item item)
-        {
-            if (item == null)
-                return;
-
-            // This will push the ItemDetailPage onto the navigation stack
-            await Shell.Current.GoToAsync($"{nameof(ItemDetailPage)}?{nameof(ItemDetailViewModel.ItemId)}={item.Id}");
-        }
     }
 }
--- App_Final/App_Final/Views/ItemsPage.xaml.cs
+++ App_Final/App_Final/Views/ItemsPage.xaml.cs
@@ -14,19 +14,11 @@ namespace App_Final.Views
 {
     public partial class ItemsPage : ContentPage
     {
-        ItemsViewModel _viewModel;
 
         public ItemsPage()
         {
             InitializeComponent();
 
-            BindingContext = _viewModel = new ItemsViewModel();
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            _viewModel.OnAppearing();
         }
     }
 }
